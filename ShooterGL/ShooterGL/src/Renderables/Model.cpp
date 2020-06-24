b07ad96568d89bd4c832ae920877a1be28ecef69
rlindsa@masonlive.gmu.edu <rlindsa@masonlive.gmu.edu>
2020-06-24 17:06:24 -0400

    Cleanup before proceeding with multiple mesh loading bug

--- ShooterGL/ShooterGL/src/Renderables/Model.cpp
+++ ShooterGL/ShooterGL/src/Renderables/Model.cpp
@@ -97,83 +97,6 @@ void Model::ProcessNode(aiNode * node, const aiScene * scene, std::string materi
 
 Mesh Model::ProcessMesh(aiMesh * mesh, const aiScene * scene, char* materialPath)
 {
-	//// data to fill
-	//std::vector<Vertex> vertices;
-	//std::vector<unsigned int> indices;
-	//std::vector<Texture> textures;
-	//
-	//// walk through each of the mesh's vertices
-	//for (unsigned int i = 0; i < mesh->mNumVertices; i++)
-	//{
-	//	Vertex vertex;
-	//	glm::vec3 vector; // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
-	//	// positions
-	//	vector.x = mesh->mVertices[i].x;
-	//	vector.y = mesh->mVertices[i].y;
-	//	vector.z = mesh->mVertices[i].z;
-	//	vertex.Position = vector;
-	//	// normals
-	//	vector.x = mesh->mNormals[i].x;
-	//	vector.y = mesh->mNormals[i].y;
-	//	vector.z = mesh->mNormals[i].z;
-	//	vertex.Normal = vector;
-	//	// texture coordinates
-	//	if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
-	//	{
-	//		glm::vec2 vec;
-	//		// a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
-	//		// use models where a vertex can have multiple texture coordinates so we always take the first set (0).
-	//		vec.x = mesh->mTextureCoords[0][i].x;
-	//		vec.y = mesh->mTextureCoords[0][i].y;
-	//		vertex.TexCoords = vec;
-	//	}
-	//	else
-	//		vertex.TexCoords = glm::vec2(0.0f, 0.0f);
-	//	// tangent
-	//	vector.x = mesh->mTangents[i].x;
-	//	vector.y = mesh->mTangents[i].y;
-	//	vector.z = mesh->mTangents[i].z;
-	//	vertex.Tangent = vector;
-	//	// bitangent
-	//	vector.x = mesh->mBitangents[i].x;
-	//	vector.y = mesh->mBitangents[i].y;
-	//	vector.z = mesh->mBitangents[i].z;
-	//	vertex.Bitangent = vector;
-	//	vertices.push_back(vertex);
-	//}
-	//// now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
-	//for (unsigned int i = 0; i < mesh->mNumFaces; i++)
-	//{
-	//	aiFace face = mesh->mFaces[i];
-	//	// retrieve all indices of the face and store them in the indices vector
-	//	for (unsigned int j = 0; j < face.mNumIndices; j++)
-	//		indices.push_back(face.mIndices[j]);
-	//}
-	//// process materials
-	////aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-	//// we assume a convention for sampler names in the shaders. Each diffuse texture should be named
-	//// as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. 
-	//// Same applies to other texture as the following list summarizes:
-	//// diffuse: texture_diffuseN
-	//// specular: texture_specularN
-	//// normal: texture_normalN
-	//
-	//// 1. diffuse maps
-	////vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
-	////textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-	//// 2. specular maps
-	////vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
-	////textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
-	//// 3. normal maps
-	////std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
-	////textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
-	//// 4. height maps
-	////std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
-	////textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
-	//
-	//// return a mesh object created from the extracted mesh data
-	//return Mesh(m_objectManager, vertices, indices, materialPath, this);
-	//return Mesh(m_objectManager, std::vector<Vertex>(), std::vector<unsigned int>(), materialPath, this);
 	if (m_meshes.size() == m_modelData->m_meshData.size())
 		m_modelData->m_meshData.push_back(MeshData());
 	return Mesh(m_objectManager, scene, mesh, materialPath, this, &(m_modelData->m_meshData[m_meshes.size()]));
@@ -182,7 +105,6 @@ Mesh Model::ProcessMesh(aiMesh * mesh, const aiScene * scene, char* materialPath
 void Model::LoadShaders()
 {
 	for (int i = 0; i < m_meshes.size(); i++)
-	//for (Mesh mesh : m_meshes)
 	{
 		m_meshes[i].LoadShaders();
 	}
